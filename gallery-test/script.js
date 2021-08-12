66ef4c1e73a1f038638b28fe883deb9a6eff26de
enriquemosso65 <enriquemosso65@aragon.unam.mx>
2021-08-12 12:57:22 -0500

    portada responsive

--- gallery-test/script.js
+++ gallery-test/script.js
@@ -1,3 +1,23 @@
+/*const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
+const body = document.querySelector('body');
+
+//display mobile menu
+
+const mobileMenu = () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+    body.classList.toggle('active');
+
+
+};
+menu.addEventListener('click', mobileMenu);
+
+console.log("work");*/
+
+
+
 window.addEventListener("load", function () {
   gsap.registerPlugin(ScrollTrigger);
 
