cd76c30aa54e412ece4332d9499827e1f3272da5
Marco <marcomartinezservin@gmail.com>
2021-08-26 16:52:35 -0500

    modlaes del artista mauricio vega agregados

---
+++ js/artistas.js
@@ -0,0 +1,63 @@
+const p1 = document.querySelector(".p1");
+p1.addEventListener("click", () => {
+  const options = {
+    showCloseButton: true,
+    showConfirmButton:  false,
+    imageUrl: '../images/3-mauricio-vega/Pintura 7-min.JPG',
+    imageWidth: 'auto',
+  imageHeight: 'auto',
+    width:"100%",
+    height:"100vh",
+    backdrop: true,
+    background: '#fff0',
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+  };
+  Swal.fire(options);
+});
+const p2 = document.querySelector(".p2");
+p2.addEventListener("click", () => {
+  const options = {
+    showCloseButton: true,
+    showConfirmButton:  false,
+    imageUrl: '../images/3-mauricio-vega/Pintura 8-min.JPG',
+    imageWidth: 'auto',
+  imageHeight: 'auto',
+    width:"100%",
+    height:"100vh",
+    backdrop: true,
+    background: '#fff',
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+  };
+  Swal.fire(options);
+});
+const p3 = document.querySelector(".p3");
+p3.addEventListener("click", () => {
+  const options = {
+    showCloseButton: true,
+    showConfirmButton:  false,
+    imageUrl: '../images/3-mauricio-vega/Tríptico completo-min.png',
+    imageWidth: 'auto',
+  imageHeight: 'auto',
+    width:"100%",
+    height:"100vh",
+    backdrop: true,
+    background: '#fff0',
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+  };
+  Swal.fire(options);
+});
