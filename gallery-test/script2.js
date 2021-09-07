042ae8506c1ecda28838321364967952e5f641e0
enriquemosso65 <enriquemosso65@aragon.unam.mx>
2021-09-07 18:14:45 -0500

    transicion lista

--- gallery-test/script2.js
+++ gallery-test/script2.js
@@ -1,8 +1,17 @@
-window.onload = function(){
+const link = document.querySelector(".link");
+const transition = document.querySelector(".transition");
+link.addEventListener("click", (e) => {
+  e.preventDefault();
+  transition.classList.add("slide");
+  setTimeout(() => {
+    window.location = link.href;
+  }, 900);
+});
+/* window.onload = function(){
  
   $('#onload').fadeOut();
   $('body').addClass('hidden');
-}  
+}  */
 /*window.onload = () => {
   const anchors = document.querySelectorAll('a');
   const transition_el = document.querySelector('.transition');
